54bbbe61b04486fbcd6823c2b4d1d6774beead60
Iris <iris3455@gmail.com>
2023-02-27 22:20:14 +0900

    Feat : validation id&PW
    Navigate to InterestScreen

--- lib/features/authentication/widgets/login_form_screen.dart
+++ lib/features/authentication/widgets/login_form_screen.dart
@@ -5,6 +5,7 @@ import 'package:tikntok/features/authentication/widgets/form_button.dart';
 
 import '../../../constants/gaps.dart';
 import '../../../constants/sizes.dart';
+import '../../onbording/interests_screen.dart';
 
 class LoginFormScreen extends StatefulWidget {
   const LoginFormScreen({super.key});
@@ -22,61 +23,78 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
     if (_formKey.currentState != null) {
       if (_formKey.currentState!.validate()) {
         _formKey.currentState!.save();
-        print(formData.values);
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: ((context) => const InterestsScreen()),
+          ),
+        );
       }
       ;
     }
   }
 
+  void _onScaffoldTap() {
+    FocusScope.of(context).unfocus();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Log in'),
-      ),
-      body: Padding(
-        padding: EdgeInsets.symmetric(
-          horizontal: Sizes.size36,
+    return GestureDetector(
+      onTap: _onScaffoldTap,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Log in'),
         ),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              Gaps.v28,
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'Email',
+        body: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: Sizes.size36,
+          ),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                Gaps.v28,
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Email',
+                  ),
+                  validator: (value) {
+                    if (value != null && value.isEmpty) {
+                      return "Please write your Email";
+                    }
+                    return null;
+                  },
+                  onSaved: (newValue) {
+                    if (newValue != null) {
+                      formData['email'] = newValue;
+                    }
+                  },
                 ),
-                validator: (value) {
-                  return null;
-                },
-                onSaved: (newValue) {
-                  if (newValue != null) {
-                    formData['email'] = newValue;
-                  }
-                },
-              ),
-              Gaps.v16,
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'Password',
+                Gaps.v16,
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                  ),
+                  validator: (value) {
+                    if (value != null && value.isEmpty) {
+                      return "Please write your Password";
+                    }
+                    return null;
+                  },
+                  onSaved: (newValue) {
+                    if (newValue != null) {
+                      formData['password'] = newValue;
+                    }
+                    ;
+                  },
                 ),
-                validator: (value) {
-                  return null;
-                },
-                onSaved: (newValue) {
-                  if (newValue != null) {
-                    formData['password'] = newValue;
-                  }
-                  ;
-                },
-              ),
-              Gaps.v28,
-              GestureDetector(
-                onTap: _onSubmitTap,
-                child: Formbutton(disabled: false, btnText: "login"),
-              ),
-            ],
+                Gaps.v28,
+                GestureDetector(
+                  onTap: _onSubmitTap,
+                  child: Formbutton(disabled: false, btnText: "login"),
+                ),
+              ],
+            ),
           ),
         ),
       ),
