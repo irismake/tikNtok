1c659ac499ea7bd11c9f54ccdeff16699a821415
Iris <iris3455@gmail.com>
2023-03-05 14:53:29 +0900

    Style : BottomNavigation bar
    BottomNavigation bar UI

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -1,12 +1,83 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class MainNavigationScreen extends StatelessWidget {
-  const MainNavigationScreen({super.key});
+class MainNavigationScreen extends StatefulWidget {
+  MainNavigationScreen({super.key});
+
+  @override
+  State<MainNavigationScreen> createState() => _MainNavigationScreenState();
+}
+
+class _MainNavigationScreenState extends State<MainNavigationScreen> {
+  int _selectedIndex = 0;
+
+  final screens = [
+    const Center(
+      child: Text('Home'),
+    ),
+    const Center(
+      child: Text('Search'),
+    ),
+    const Center(
+      child: Text('Home'),
+    ),
+    const Center(
+      child: Text('Search'),
+    ),
+    const Center(
+      child: Text('Home'),
+    ),
+  ];
+
+  void _onTap(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onTap,
+        //selectedItemColor: Theme.of(context).primaryColor,
+        items: const [
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.house),
+            label: "Home",
+            tooltip: "What are you?",
+            backgroundColor: Colors.amber,
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
+            label: "Search",
+            tooltip: "What ate you?",
+            backgroundColor: Colors.blue,
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.house),
+            label: "Home",
+            tooltip: "What are you?",
+            backgroundColor: Colors.pink,
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
+            label: "Search",
+            tooltip: "What ate you?",
+            backgroundColor: Colors.yellow,
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
+            label: "Search",
+            tooltip: "What ate you?",
+            backgroundColor: Colors.teal,
+          ),
+        ],
+      ),
+    );
   }
 }
